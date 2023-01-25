cb048967a69d990ae7f7f4e5d59f6d0411de1c34
Sacha Trauwaen <info@satrabel.be>
2023-01-25 16:05:21 +0100

    public

--- src/Satrabel.Abp.Theme.CoreUI/CoreUITheme.cs
+++ src/Satrabel.Abp.Theme.CoreUI/CoreUITheme.cs
@@ -18,6 +18,8 @@ namespace Satrabel.Abp.Theme.CoreUI
                     return "~/Themes/CoreUI/Layouts/Application.cshtml";
                 case StandardLayouts.Account:
                     return "~/Themes/CoreUI/Layouts/Account.cshtml";
+                case StandardLayouts.Public:
+                    return "~/Themes/CoreUI/Layouts/Application.cshtml";
                 case StandardLayouts.Empty:
                     return "~/Themes/CoreUI/Layouts/Empty.cshtml";
                 default:
